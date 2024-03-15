8cd09f0cffa2d3d2a276598640446602ada511a4
Atharva Inamdar <atharvai@iitbhilai.ac.in>
2024-03-16 00:32:35 +0530

    add nvim config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,34 @@
+-- Tabs 
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Copy to clipboard
+vim.cmd("set clipboard=unnamed")
+vim.cmd("set clipboard+=unnamedplus")
+
+-- Map leader to space
+vim.g.mapleader = " "
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- Remove the highlight from the search command 
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.wo.number = true
+
+
+-- Indent/unindent with tab/shift-tab
+vim.keymap.set("n", "<Tab>", ">>")
+vim.keymap.set("n", "<S-Tab>", "<<")
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+
+
+
+
